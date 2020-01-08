7d9a637ef5119c8133c48156578e08c35efe5b7c
Kevin Liebholz <kevin@liebholz.eu>
2020-01-08 20:31:18 +0100

    FEAT: added model Ingrediet

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_08_192522) do
+ActiveRecord::Schema.define(version: 2020_01_08_193029) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,12 @@ ActiveRecord::Schema.define(version: 2020_01_08_192522) do
     t.datetime "updated_at", null: false
     t.index ["cocktail_id"], name: "index_favorites_on_cocktail_id"
     t.index ["user_id"], name: "index_favorites_on_user_id"
+  end
+
+  create_table "ingredients", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
